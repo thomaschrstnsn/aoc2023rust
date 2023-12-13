ba83c1069a7a48376f8150016d54b5b53117580f
Thomas Christensen <thomas@chrstnsn.dk>
2023-12-13 23:09:19 +0100

    day 7: part 1
    nerding Ord

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,46 +1,83 @@
-use std::{collections::HashSet, num::ParseIntError};
+use itertools::Itertools;
+use std::{cmp::Ordering, collections::HashMap, num::ParseIntError};
 
 advent_of_code::solution!(7);
 
-#[derive(Debug, Eq, PartialEq, Hash)]
+#[derive(Debug, Eq, PartialEq, PartialOrd, Hash, Ord)]
 enum Card {
-    Ace,
-    King,
-    Queen,
-    Jack,
-    Ten,
-    Nine,
-    Eight,
-    Seven,
-    Six,
-    Five,
-    Four,
-    Three,
-    Two,
-    One,
-}
-
-fn discriminator(card: &Card) -> u8 {
-    match card {
-        Card::Ace => 14,
-        Card::King => 13,
-        Card::Queen => 12,
-        Card::Jack => 11,
-        Card::Ten => 10,
-        Card::Nine => 9,
-        Card::Eight => 8,
-        Card::Seven => 7,
-        Card::Six => 6,
-        Card::Five => 5,
-        Card::Four => 4,
-        Card::Three => 3,
-        Card::Two => 2,
-        Card::One => 1,
-    }
+    Ace = 14,
+    King = 13,
+    Queen = 12,
+    Jack = 11,
+    Ten = 10,
+    Nine = 9,
+    Eight = 8,
+    Seven = 7,
+    Six = 6,
+    Five = 5,
+    Four = 4,
+    Three = 3,
+    Two = 2,
+    One = 1,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-struct Hand(HashSet<Card>);
+struct Hand(Vec<Card>);
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum HandRanking {
+    FiveOfAKind = 10,
+    FourOfAKind = 9,
+    FullHouse = 8,
+    ThreeOfAKind = 7,
+    TwoPair = 6,
+    OnePair = 5,
+    HighCard = 4,
+}
+
+fn rank(Hand(hand): &Hand) -> HandRanking {
+    use HandRanking::*;
+    let counts: HashMap<&Card, usize> = hand.into_iter().counts();
+
+    let mut freq: Vec<usize> = counts.into_values().collect();
+    freq.sort();
+
+    let mut freq_iter = freq.into_iter().rev();
+    match (freq_iter.next(), freq_iter.next()) {
+        (Some(5), _) => FiveOfAKind,
+        (Some(4), _) => FourOfAKind,
+        (Some(3), Some(2)) => FullHouse,
+        (Some(3), _) => ThreeOfAKind,
+        (Some(2), Some(2)) => TwoPair,
+        (Some(2), _) => OnePair,
+        _ => HighCard,
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let self_rank = rank(self);
+        let other_rank = rank(other);
+        match self_rank.cmp(&other_rank) {
+            Ordering::Equal => {
+                let Hand(self_cards) = self;
+                let Hand(other_cards) = other;
+                let first_diff = std::iter::zip(self_cards, other_cards)
+                    .map(|(a, b)| a.cmp(&b))
+                    .skip_while(|&o| o == Ordering::Equal)
+                    .next();
+                first_diff.unwrap_or(Ordering::Equal)
+            }
+            less_or_greater => less_or_greater,
+        }
+    }
+}
 
 type Bid = u32;
 
@@ -87,7 +124,7 @@ impl std::str::FromStr for Input {
         let hand = hand_str
             .chars()
             .map(|c| parse_card(c).ok_or(ParseError::InvalidCard(c)))
-            .collect::<Result<HashSet<_>, Self::Err>>()?;
+            .collect::<Result<Vec<_>, Self::Err>>()?;
 
         let bid = bid_str
             .parse::<u32>()
@@ -102,7 +139,17 @@ impl std::str::FromStr for Input {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let mut inputs = parse(input).expect("parsable input");
+
+    inputs.sort_by(|Input(a_hand, _), Input(b_hand, _)| a_hand.cmp(&b_hand));
+
+    Some(
+        inputs
+            .into_iter()
+            .enumerate()
+            .map(|(index, Input(_, bid))| (index as u32 + 1) * bid)
+            .sum(),
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -113,11 +160,12 @@ pub fn part_two(input: &str) -> Option<u32> {
 mod tests {
     use super::*;
     use Card::*;
+    use HandRanking::*;
 
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6440));
     }
 
     #[test]
@@ -131,10 +179,27 @@ mod tests {
         let result =
             parse(&advent_of_code::template::read_file("examples", DAY)).expect("should parse");
 
-        let expected = Input(
-            Hand(HashSet::<Card>::from([Three, Two, Ten, Three, King])),
-            765,
-        );
+        let expected = Input(Hand(vec![Three, Two, Ten, Three, King]), 765);
         assert_eq!(result[0], expected);
+    }
+
+    #[test]
+    fn test_rank() {
+        assert_eq!(rank(&Hand(vec!(Ace, Ace, Ace, Ace, Ace))), FiveOfAKind);
+        assert_eq!(rank(&Hand(vec!(One, Ace, Ace, Ace, Ace))), FourOfAKind);
+        assert_eq!(rank(&Hand(vec!(One, Ace, Ace, One, Ace))), FullHouse);
+        assert_eq!(rank(&Hand(vec!(One, Ace, Ace, Three, Ace))), ThreeOfAKind);
+        assert_eq!(rank(&Hand(vec!(One, Ace, One, Three, Ace))), TwoPair);
+        assert_eq!(rank(&Hand(vec!(One, Ace, Two, Three, Ace))), OnePair);
+        assert_eq!(rank(&Hand(vec!(One, Five, Two, Three, Ace))), HighCard);
+    }
+
+    #[test]
+    fn test_cmp_hand() {
+        assert!(Hand(vec![Ace, Ace, Ace, Ace, One]) > Hand(vec![One, Ace, Ace, Ace, Ace]));
+        assert!(Hand(vec![One, Ace, Ace, Ace, One]) < Hand(vec![Ace, Ace, Ace, Ace, One]));
+        assert!(Hand(vec![Ace, Ace, Ace, Ace, One]) > Hand(vec![One, Ace, Two, Ace, Ace]));
+        assert!(Hand(vec![Ace, Ten, King, Queen, One]) == Hand(vec![Ace, Ten, King, Queen, One]));
+        assert!(Hand(vec![One, Ace, Ace, Ace, Ace]) < Hand(vec![Three, One, One, One, One]));
     }
 }
